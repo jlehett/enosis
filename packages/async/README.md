ed69091b4852d656266e61b65edfc7c0d6d838ee
Tanner Lehett <john.cfplus@gmail.com>
2021-10-09 14:25:55 -0400

    Renovating existing codebase.

---
+++ packages/async/README.md
@@ -0,0 +1,11 @@
+# `@unifire-js/async`
+
+> TODO: description
+
+## Usage
+
+```
+const async = require('@unifire-js/async');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/async/__tests__/deferred.test.js
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import { Deferred } from '../lib/index';
+
+describe('Deferred Object', () => {
+
+    it('is able to be resolved from external scope', async () => {
+        const deferred = new Deferred();
+        setTimeout(() => {
+            deferred.resolve(5);
+        }, 250);
+        const value = await deferred.promise;
+        expect(value).to.equal(5);
+    });
+
+    it('is able to be rejected from external scope', async () => {
+        const deferred = new Deferred();
+        setTimeout(() => {
+            deferred.reject(5);
+        }, 250);
+        try {
+            await deferred.promise;
+        } catch (err) {
+            expect(err).to.equal(5);
+        }
+    });
+
+});
