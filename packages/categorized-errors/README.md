b0496f0cc186967994dba71c8fea5a9342b94687
Tanner Lehett <john.cfplus@gmail.com>
2021-10-10 03:45:22 -0400

    Port over old categorized error codebase

---
+++ packages/categorized-errors/README.md
@@ -0,0 +1,11 @@
+# `categorized-errors`
+
+> TODO: description
+
+## Usage
+
+```
+const categorizedErrors = require('categorized-errors');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/categorized-errors/src/index.js
@@ -0,0 +1,4 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+export { default as CategorizedErrorFactory } from './categorized-error-factory/categorized-error-factory';
